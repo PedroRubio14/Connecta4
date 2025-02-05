4c055c745aa42856d232d3187eff847f93af4ce8
Pedro <pedrorubiorigo@gmail.com>
2025-02-05 18:11:21 +0100

    prova

--- src/Tablero.java
+++ src/Tablero.java
@@ -7,5 +7,16 @@ public class Tablero {
         tablero = new char [filas][columnas];
     }
 
+    public void mostrarTablero() {
+        for (int i = 0; i < tablero.length; i++) {
+            for (int y = 0; y < tablero[i].length; y++) {
+                System.out.print("[" + tablero[i][y] + "]");
+            }
+            System.out.println();
+        }
+    }
+
+
+
 
 }
