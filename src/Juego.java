224d9e5e1b2d722d0086a0484de4a4949a111390
Pedro <pedrorubiorigo@gmail.com>
2025-02-06 12:17:15 +0100

    Empate

--- src/Juego.java
+++ src/Juego.java
@@ -7,24 +7,21 @@ public class Juego {
         Juego j = new Juego();
         Jugador O = nombreJugadorO();
         Jugador X = nombreJugadorX();
-        while (!laPartidaHaTerminado(esGanador(),empate())){
+        while (!laPartidaHaTerminado(esGanador(),empate(tablero))){
             tablero.mostrarTablero();
             ponerFicha(O,tablero);
             tablero.mostrarTablero();
-            if (laFichaEraGanadora()){
+            if (laPartidaHaTerminado(esGanador(),empate(tablero))){
                 break;
             }
             ponerFicha(X,tablero);
             tablero.mostrarTablero();
         }
-
+        System.out.print("La partida ha finalizado");
 
     }
 
     public Jugador nombreJugadorO (){
-        System.out.println("Nombre del Jugador X: ");
-        String nombreX = sc.nextLine();
-        Jugador jugadorX = new Jugador(nombreX,'X');
         System.out.println("Nombre del Jugador O: ");
         String nombreO = sc.nextLine();
         Jugador jugadorO = new Jugador(nombreO,'O');
@@ -56,12 +53,21 @@ public class Juego {
         return false;
     }
 
-    private boolean empate (){
-        return false;
+    private boolean empate (Tablero t){
+        char [][] tablero = t.getTablero();
+        if(!esGanador()) {
+            for (int i = 0; i < tablero.length; i++) {
+                for (int y = 0; y < tablero[i].length; y++) {
+                    if (tablero[i][y] == 0) {
+                        return false;
+                    }
+                }
+            }
+            System.out.println("EMPATE!!");
+            return true;
+        }
     }
 
-    private boolean laFichaEraGanadora(){
-        return false;
-    }
+
 
 }
--- src/Tablero.java
+++ src/Tablero.java
@@ -16,7 +16,7 @@ public class Tablero {
             }
             System.out.println();
         }
-        System.out.println(" 1  2  3  4  5  6  7");
+        System.out.println(" 0  1  2  3  4  5  6");
         System.out.println();
         System.out.println();
     }
