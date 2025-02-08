f3f8c4d4d6a155bbcad5688a3b95ed87d412fb31
Pedro <pedrorubiorigo@gmail.com>
2025-02-08 16:31:31 +0100

    ganador

--- src/Juego.java
+++ src/Juego.java
@@ -113,7 +113,7 @@ public class Juego {
     private boolean filaGanadora(char ficha, char [][] t){
         for(int i =0; i < t.length; i++){
             for (int y = 0; y<t[i].length-3;y++){
-                if(t[i][y]==ficha&&t[i][y+1]==ficha&&t[i][y+2]==ficha&&t[i][y+3]==ficha){
+                if(t[i][y] == ficha && t[i][y+1] == ficha && t[i][y+2] == ficha && t[i][y+3] == ficha){
                     return true;
                 }
             }
@@ -124,7 +124,7 @@ public class Juego {
     private boolean columnaGanadora(char ficha, char [][] t){
         for(int i = 0; i<t[0].length;i++){
             for(int y =0; y<t.length-3;y++){
-                if(t[y][i]==ficha&&t[y+1][i]==ficha&&t[y+2][i]==ficha&&t[y+3][i]==ficha){
+                if(t[y][i] == ficha && t[y+1][i] == ficha && t[y+2][i] == ficha && t[y+3][i] == ficha){
                     return true;
                 }
 
@@ -135,8 +135,32 @@ public class Juego {
 
     }
 private boolean ganadorDiagonal(char ficha, char [][] t){
+    return diagonalIzquierda(ficha, t) || digonalDerecha(ficha, t);
+
+}
+
+private boolean diagonalIzquierda(char ficha, char[][] t){
+
+    for (int y = 0; y < t.length-3; y++) {
+        for (int x = 0; x <t[0].length-3; x++) {
+            if (t[y][x] == ficha && t[y+1][x+1] == ficha && t[y+2][x+2] == ficha && t[y+3][x+3] == ficha) {
+                return true;
+            }
+        }
+    }
     return false;
 
+}
+private boolean digonalDerecha (char ficha, char[][] t){
+    for (int y = 3; y < t.length; y++) {
+        for (int x = 0; x < t[0].length - 3; x++) {
+            if (t[y][x] == ficha && t[y-1][x+1] == ficha && t[y-2][x+2] == ficha && t[y-3][x+3] == ficha) {
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
 
 }
